3b24b92c897f973e1cbf1d8a04cf889d35cecb07
KeTuFaisPiKiNut <lavoieleon@gmail.com>
2021-11-11 13:17:51 -0500

    duck

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="TeleOp")
+@TeleOp(name="TeleOp",group="duck")
 public class MainOpMode extends OpMode {
     @Override
     public void init() {
-
+        System.out.println("Init ");
     }
 
     @Override
